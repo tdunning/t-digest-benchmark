2d0dd76c465634c67bc46828dfdb5fd8bff5b241
Ted Dunning <tdunning@apache.org>
2014-05-12 22:21:44 -0700

    Updated directions, added visualization code

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ t-digest-benchmark
 ==================
 
 A simple JMH benchmark for various versions of t-digest
+
+How to Run
+==========
+
+Compile the code
+
+    mvn clean install
+
+and run the benchmarks
+
+    java -cp target/microbenchmarks.jar:target/target/t-digest-benchmark-1.0-SNAPSHOT.jar com.tdunning.Benchmark
+
+Then run the visualization
+
+    Rscript plot-data.r
+
+Look at plot.pdf for the results.
---
+++ src/main/java/com/tdunning/benchmark.java
@@ -0,0 +1,85 @@
+package com.tdunning;
+
+import com.tdunning.math.stats.AVLTreeDigest;
+import com.tdunning.math.stats.ArrayDigest;
+import com.tdunning.math.stats.TDigest;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.output.results.ResultFormatType;
+import org.openjdk.jmh.profile.ProfilerType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 3, time = 3, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@Threads(1)
+@State(Scope.Thread)
+public class Benchmark {
+    private Random gen = new Random();
+    private double[] data;
+
+    @Param({"20", "50", "100", "200", "500"})
+    public int compression;
+
+    @Param({"tree", "array"})
+    public String method;
+
+    private TDigest td;
+
+    @Setup
+    public void setup() {
+        data = new double[10000000];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = gen.nextDouble();
+        }
+        if (method.equals("tree")) {
+            td = new AVLTreeDigest(compression);
+        } else {
+            td = new ArrayDigest(32, compression);
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class ThreadState {
+        int index = 0;
+    }
+
+    @GenerateMicroBenchmark
+    public void add(ThreadState state) {
+        if (state.index >= data.length) {
+            state.index = 0;
+        }
+        td.add(data[state.index++]);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(".*" + Benchmark.class.getSimpleName() + ".*")
+                .resultFormat(ResultFormatType.CSV)
+                .result("results.csv")
+                .addProfiler(ProfilerType.HS_GC)
+                .addProfiler(ProfilerType.HS_RT)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
